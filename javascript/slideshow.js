da863d7845f852c09c4d5b552ba1a541e8e5ec1e
justin leggett <justin@codingrookie.com>
2016-10-04 21:10:09 -0500

    completed the fadein/slidein checkbox

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,19 +1,42 @@
 "use strict";
+
+// 1. Use JQuery AJAX to get your images from an external API. Try NASA's Astronomy Picture of the Day API or find your own.
+// 2. Allow the user to change the animation from fade in/out to slide in/out.
+// 3. Have captions only appear on hover.
 var $slideshow = $('#slideshow');
 var $slideShowBtn = $('#slideshowbtn');
 var counter = 0;
 var isRunning = false;
 var intv;
-
 var caption = ["Caption #0", "Caption #1", "Caption #2", "Caption #3",];
+var images = [""];
+
+// ajax call to an external source
+// $.get( "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", function( nasa ) {
+//   console.log( "nasa Loaded: " , images = nasa.hdurl );
+// });
+
 // display image
 function displayImg(imgNum) {
-    $slideshow.fadeOut("slow", function() {
-        $slideshow.attr('src', `img/code${imgNum}.jpg`);
-        $('#caption').html(caption[imgNum]);
-    });
-    $slideshow.fadeIn("slow");
+    // checking if box is checked
+    var isChecked = $('#slidein')[0];
+    if(isChecked.checked) {
+        console.log("its clicked");
+        $slideshow.animate({ width: "0%" }, {duration: 2000, complete: function() {
+            console.log("done!!!");
+            $slideshow.attr('src', `img/code${imgNum}.jpg`);
+            $('#caption').html(caption[imgNum]);
+        }});
+            $slideshow.animate({ width: "100%" }, 2000 );
+    } else {
+        $slideshow.fadeOut("slow", function() {
+            $slideshow.attr('src', `img/code${imgNum}.jpg`);
+            $('#caption').html(caption[imgNum]);
+        });
+        $slideshow.fadeIn("slow");
+    }
 }
+
 
 // AUTO SLIDE
 function auto(){
@@ -24,10 +47,9 @@ intv = setInterval(function() {
             counter++;
         }
         displayImg(counter);
-        console.log("counter", counter);
         isRunning = true;
         // reset counter
-   }, 2000 );
+   }, 2000);
 }
 
 // When SlideShow Button is clicked it auto plays
@@ -43,6 +65,14 @@ $slideShowBtn.click(function() {
     }
 });
 
+
+// Event listeners
+// Listener for fade in effect
+$('#fadein').on('change', function() {
+    if(this.checked) {
+        console.log("Checkbox is checked");
+    }
+});
 
 // prev and next also stop the slideshow if it is running
 $('#prev').click(function() {
@@ -68,3 +98,4 @@ $('#next').click(function(e) {
     $slideShowBtn.val("Start SlideShow");
     displayImg(counter);
 });
+
